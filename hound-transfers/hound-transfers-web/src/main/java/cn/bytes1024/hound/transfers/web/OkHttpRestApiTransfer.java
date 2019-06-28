76ce4a6550bb20b00dd8e8983c4cbb0f65011ce7
江浩 <jh_vip_1024@163.com>
2019-06-28 14:19:40 +0800

    # 暂存

---
+++ hound-transfers/hound-transfers-web/src/main/java/cn/bytes1024/hound/transfers/web/OkHttpRestApiTransfer.java
@@ -0,0 +1,15 @@
+package cn.bytes1024.hound.transfers.web;
+
+import cn.bytes1024.hound.transfers.define.TransmitObject;
+
+/**
+ * rest api 数据传输器
+ *
+ * @author 江浩
+ */
+public class OkHttpRestApiTransfer extends AbstractRestApiTransfer {
+    @Override
+    public void transmit(TransmitObject transmitObject) {
+
+    }
+}
